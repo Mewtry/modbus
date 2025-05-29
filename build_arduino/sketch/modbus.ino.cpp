fcee8c3f0ec69d6df5ce475bb5eb36412396ca2e
Mewtry <theo.v.p.com@gmail.com>
2025-05-29 15:25:26 -0300

    Feat: Todas funções funcionais

--- build_arduino/sketch/modbus.ino.cpp
+++ build_arduino/sketch/modbus.ino.cpp
@@ -1,48 +1,56 @@
+#line 1 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 #include <Arduino.h>
-#line 1 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+
 #define SLAVE_ID 1
 #define TX_ENABLE_PIN 2
 #define BAUD_RATE 9600
 
 
-#define NUMCOILS 6
-uint8_t coils[NUMCOILS] = {13, 12, 11, 10, 9, 8};
+#define NUM_COILS 6
+uint8_t coils[NUM_COILS] = {8, 9, 10, 11, 12, 13}; // Pinos D8 a D13 | PB0 a PB5
+
+#define NUM_DISCRETE_INPUTS 6
+uint8_t inputs[NUM_DISCRETE_INPUTS] = {2, 3, 4, 5, 6, 7}; // Pinos D2 a D7 | PD2 a PD7
+
+#define NUM_INPUT_REGISTERS 6
+uint16_t inputRegisters[NUM_INPUT_REGISTERS] = {14, 15, 16, 17, 18, 19}; // Pinos D14 a D19 | PC0 a PC5
 
 #define NUM_REGISTERS 10
 uint16_t holdingRegisters[NUM_REGISTERS] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 
-#line 12 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 20 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 void setup();
-#line 25 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 30 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 void loop();
-#line 85 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 97 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 bool handleReadCoils(uint8_t* request, uint8_t* response, uint8_t* responseLength);
-#line 116 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
-bool handleModbusFunctionException(uint8_t functionCode, uint8_t* response, uint8_t* responseLength);
-#line 129 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 123 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
+bool handleReadDiscreteInputs(uint8_t* request, uint8_t* response, uint8_t* responseLength);
+#line 148 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 bool handleReadHoldingRegisters(uint8_t* request, uint8_t* response, uint8_t* responseLength);
-#line 153 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 173 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
+bool handleReadInputRegisters(uint8_t* request, uint8_t* response, uint8_t* responseLength);
+#line 201 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 bool handleWriteSingleCoil(uint8_t* request, uint8_t* response, uint8_t* responseLength);
-#line 190 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 233 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 bool handleWriteSingleRegister(uint8_t* request, uint8_t* response, uint8_t* responseLength);
-#line 208 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 254 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
+bool handleModbusFunctionException(uint8_t functionCode, uint8_t* response, uint8_t* responseLength);
+#line 267 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 uint16_t calculateCRC(uint8_t *data, uint8_t length);
-#line 223 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 282 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 bool verifyCRC(uint8_t* data, uint8_t len, uint8_t crcMSB, uint8_t crcLSB);
-#line 234 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 293 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 void sendModbusResponse(uint8_t* data, uint8_t len);
-#line 12 "C:\\workspace\\ifsc\\modbus\\modbus\\modbus.ino"
+#line 20 "C:\\workspace\\IFSC\\RIS\\modbus\\modbus\\modbus.ino"
 void setup() {
   pinMode(TX_ENABLE_PIN, OUTPUT);
   digitalWrite(TX_ENABLE_PIN, LOW);
-  // for (int i = 0; i < NUMCOILS; i++) {
-  //   pinMode(coils[i], OUTPUT);
-  //   digitalWrite(coils[i], LOW); // Inicializa bobinas como desligadas
-  // }
-  DDRB = 0b00111111; // Pinos de D8 a D13 como saída (PB0 a PB5)
-  PORTB = 0b00100000; // LED D13 Ligado
+  DDRB |= 0b00111111; // Pinos de D8 a D13 como saída (PB0 a PB5)
+  DDRC = 0b00000000;  // Pinos de D14 a D19 como entrada (PC0 a PC5)
+  DDRD = 0b00000000;  // Pinos de D2 a D7 como entrada (PD2 a PD7)
+  // PORTB |= 0b00100000; // LED D13 Ligado
   Serial.begin(BAUD_RATE);
-  // Serial.println("Initialized");
 }
 
 void loop() {
@@ -85,8 +93,14 @@ void loop() {
       case 0x01:
         if (!handleReadCoils(request, response, &responseLength)) return;
         break;
+      case 0x02:
+        if (!handleReadDiscreteInputs(request, response, &responseLength)) return;
+        break;
       case 0x03:
         if (!handleReadHoldingRegisters(request, response, &responseLength)) return;
+        break;
+      case 0x04:
+        if (!handleReadInputRegisters(request, response, &responseLength)) return;
         break;
       case 0x05:
         if (!handleWriteSingleCoil(request, response, &responseLength)) return;
@@ -98,37 +112,31 @@ void loop() {
         if (!handleModbusFunctionException(request[1], response, &responseLength)) return;
         break; 
     }
-
     sendModbusResponse(response, responseLength);
   }
 }
 
 // Função 0x01 - Read Coils 
-// Exemplo de requisição: 01 01 00 01 00 05 C9 AD
+// Exemplo de requisição: 01 01 00 01 00 06 C8 ED | Lê todas as 6 bobinas 
+// Exemplo de requisição: 01 01 00 03 00 04 C9 CD | Lê as 4 últimas bobinas 
+// Exemplo de requisição: 01 01 00 01 00 03 CB 2D | Lê as 3 primeiras bobinas 
 bool handleReadCoils(uint8_t* request, uint8_t* response, uint8_t* responseLength) { 
   uint16_t startAddr = (request[2] << 8) | request[3];
   uint16_t numCoils  = (request[4] << 8) | request[5];
   if(0x0001 > numCoils || numCoils > 0x07D0) return false; // Número de bobinas inválido | CodeError 0x03
-  if(startAddr + numCoils > NUM_REGISTERS) return false;   // Leitura de coils inválida  | CodeError 0x02
+  if((startAddr + numCoils - 1) > NUM_COILS) return false;   // Leitura de coils inválida  | CodeError 0x02
 
   response[0] = SLAVE_ID;
   response[1] = 0x01; // Função de leitura de bobinas
-  response[2] = numCoils / 8 + (numCoils % 8 ? 1 : 0); // Número de bytes a serem enviados
+  response[2] = 1;    // Número de bytes a serem enviados
+  response[3] = 0x00; // Inicializa o primeiro byte de resposta
 
   uint8_t val = 0;
-  PORTB = 0x00010101;
   for(int i = 0; i < numCoils; i++) {
     val = digitalRead(coils[(startAddr - 1) + i]);
-    Serial.print("Pino ");
-    Serial.print(coils[(startAddr - 1) + i]);
-    Serial.print(": ");
-    Serial.println(val);
-    response [3 + i / 8] |= (val << (i % 8));
+    response [3] |= (val << i);
   }
-
-  Serial.print("Response: ");
-  Serial.println(response[3], HEX);
-
+  
   uint16_t crc = calculateCRC(response, 3 + response[2]);
   response[3 + response[2]] = crc & 0xFF;
   response[4 + response[2]] = crc >> 8;
@@ -136,15 +144,29 @@ bool handleReadCoils(uint8_t* request, uint8_t* response, uint8_t* responseLengt
   return true;
 }
 
-bool handleModbusFunctionException(uint8_t functionCode, uint8_t* response, uint8_t* responseLength) {
+// Função 0x02 - Read Discrete Inputs
+// Exemplo de requisição: 01 02 00 00 00 06 08 F8 | Lê todos os 6 inputs digitais
+bool handleReadDiscreteInputs(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
+  uint16_t startAddr = (request[2] << 8) | request[3];
+  uint16_t numInputs  = (request[4] << 8) | request[5];
+  if(0x0000 > numInputs || numInputs > 0x07D0) return false; // Número de inputs inválido 
+  if((startAddr + numInputs) > NUM_DISCRETE_INPUTS) return false;   // Leitura de inputs inválida
+
   response[0] = SLAVE_ID;
-  response[1] = functionCode | 0x80;
-  response[2] = 0x01;
+  response[1] = 0x02; // Função de leitura de inputs digitais
+  response[2] = 0x01; // Número de bytes a serem enviados
+  response[3] = 0x00; // Inicializa o primeiro byte de resposta
   
-  uint16_t crc = calculateCRC(response, 3);
-  response[3] = crc & 0xFF;
-  response[4] = crc >> 8;
-  *responseLength = 5;
+  uint8_t val = 0;
+  for(int i = 0; i < numInputs; i++) {
+    val = digitalRead(inputs[(startAddr) + i]);
+    response [3] |= (val << i);
+  }
+
+  uint16_t crc = calculateCRC(response, 3 + response[2]);
+  response[3 + response[2]] = crc & 0xFF;
+  response[4 + response[2]] = crc >> 8;
+  *responseLength = 5 + response[2];
   return true;
 }
 
@@ -171,25 +193,50 @@ bool handleReadHoldingRegisters(uint8_t* request, uint8_t* response, uint8_t* re
   return true;
 }
 
+// Função 0x04 - Read Input Registers
+// Exemplo de requisição: 01 04 00 00 00 01 CA 31 | Lê o registrador de entrada A0
+// Exemplo de requisição: 01 04 00 00 00 06 08 70 | Lê os registradores de entrada A0 a A5
+bool handleReadInputRegisters(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
+  uint16_t startAddr = (request[2] << 8) | request[3];
+  uint16_t numInputs  = (request[4] << 8) | request[5];
+  if(0x0000 > numInputs || numInputs > 0x07D0) return false; // Número de inputs inválido
+  if((startAddr + numInputs) > NUM_INPUT_REGISTERS) return false;   // Leitura de inputs inválida
+
+  response[0] = SLAVE_ID;
+  response[1] = 0x04; // Função de leitura de registradores de entrada
+  response[2] = numInputs * 2; // Número de bytes a serem enviados
+
+  for (int i = 0; i < numInputs; i++) {
+    uint16_t val = analogRead(inputRegisters[startAddr + i]);
+    response[3 + 2 * i] = val >> 8;
+    response[4 + 2 * i] = val & 0xFF;
+  }
+
+  uint16_t crc = calculateCRC(response, 3 + response[2]);
+  response[3 + response[2]] = crc & 0xFF;
+  response[4 + response[2]] = crc >> 8;
+  *responseLength = 5 + response[2];
+  return true;
+}
+
 // Função 0x05 - Write Single Coil
-// Exemplo de requisição: 01 05 00 00 FF 00 3A 8C
+// Exemplo de requisição: 01 05 00 00 FF 00 3A 8C | Liga o PB0 (D8)
+// Exemplo de requisição: 01 05 00 00 00 00 CA CD | Desliga o PB0 (D8)
+// Exemplo de requisição: 01 05 00 05 FF 00 3B 9C | Liga o PB5 (D13)
+// Exemplo de requisição: 01 05 00 05 00 00 CB DD | Desliga o PB5 (D13)
 bool handleWriteSingleCoil(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
   uint16_t coilAddr = (request[2] << 8) | request[3]; 
   uint16_t value    = (request[4] << 8) | request[5];
-  // Serial.print("Coil Addr: ");
-  // Serial.println(coilAddr, HEX);
-  // Serial.print("Value: ");
-  // Serial.println(value, HEX);
-  if (coilAddr >= NUMCOILS) return false;
+
+  if (coilAddr >= NUM_COILS) return false;
   if (value == 0x0000) {
-    value = 0; // Desliga a bobina
+    value = 0; 
   } else if (value == 0xFF00) {
-    value = 1; // Liga a bobina
+    value = 1;
   } else {
     return false; // Valor inválido
   }
-  // Serial.print("Value: ");
-  // Serial.println(value);
+
   // Atualiza o estado da bobina
   digitalWrite(coils[coilAddr], value);
   if(digitalRead(coils[coilAddr]) != value) {
@@ -206,14 +253,17 @@ bool handleWriteSingleCoil(uint8_t* request, uint8_t* response, uint8_t* respons
   response[7] = crc >> 8;
   *responseLength = 8;
   return true;
-
 }
 
 // Função 0x06 - Write Single Register
 bool handleWriteSingleRegister(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
   uint16_t regAddr = (request[2] << 8) | request[3]; 
   uint16_t value   = (request[4] << 8) | request[5];
-  if (regAddr >= NUM_REGISTERS) return false;  
+  if (regAddr >= NUM_REGISTERS) return false;
+  if (value < 0 || value > 0xFFFF) return false; // Valor inválido
+
+  // Atualiza o valor do registrador
+  holdingRegisters[regAddr] = value;
 
   // Eco da requisição como resposta
   for (int i = 0; i < 6; i++) {
@@ -224,6 +274,18 @@ bool handleWriteSingleRegister(uint8_t* request, uint8_t* response, uint8_t* res
   response[6] = crc & 0xFF;
   response[7] = crc >> 8;
   *responseLength = 8;
+  return true;
+}
+
+bool handleModbusFunctionException(uint8_t functionCode, uint8_t* response, uint8_t* responseLength) {
+  response[0] = SLAVE_ID;
+  response[1] = functionCode | 0x80;
+  response[2] = 0x01;
+  
+  uint16_t crc = calculateCRC(response, 3);
+  response[3] = crc & 0xFF;
+  response[4] = crc >> 8;
+  *responseLength = 5;
   return true;
 }
 
--- modbus/modbus.ino
+++ modbus/modbus.ino
@@ -1,10 +1,18 @@
+#include <Arduino.h>
+
 #define SLAVE_ID 1
 #define TX_ENABLE_PIN 2
 #define BAUD_RATE 9600
 
 
-#define NUMCOILS 6
-uint8_t coils[NUMCOILS] = {13, 12, 11, 10, 9, 8};
+#define NUM_COILS 6
+uint8_t coils[NUM_COILS] = {8, 9, 10, 11, 12, 13}; // Pinos D8 a D13 | PB0 a PB5
+
+#define NUM_DISCRETE_INPUTS 6
+uint8_t inputs[NUM_DISCRETE_INPUTS] = {2, 3, 4, 5, 6, 7}; // Pinos D2 a D7 | PD2 a PD7
+
+#define NUM_INPUT_REGISTERS 6
+uint16_t inputRegisters[NUM_INPUT_REGISTERS] = {14, 15, 16, 17, 18, 19}; // Pinos D14 a D19 | PC0 a PC5
 
 #define NUM_REGISTERS 10
 uint16_t holdingRegisters[NUM_REGISTERS] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
@@ -12,14 +20,11 @@ uint16_t holdingRegisters[NUM_REGISTERS] = {100, 200, 300, 400, 500, 600, 700, 8
 void setup() {
   pinMode(TX_ENABLE_PIN, OUTPUT);
   digitalWrite(TX_ENABLE_PIN, LOW);
-  // for (int i = 0; i < NUMCOILS; i++) {
-  //   pinMode(coils[i], OUTPUT);
-  //   digitalWrite(coils[i], LOW); // Inicializa bobinas como desligadas
-  // }
-  DDRB = 0b00111111; // Pinos de D8 a D13 como saída (PB0 a PB5)
-  PORTB = 0b00100000; // LED D13 Ligado
+  DDRB |= 0b00111111; // Pinos de D8 a D13 como saída (PB0 a PB5)
+  DDRC = 0b00000000;  // Pinos de D14 a D19 como entrada (PC0 a PC5)
+  DDRD = 0b00000000;  // Pinos de D2 a D7 como entrada (PD2 a PD7)
+  // PORTB |= 0b00100000; // LED D13 Ligado
   Serial.begin(BAUD_RATE);
-  // Serial.println("Initialized");
 }
 
 void loop() {
@@ -62,8 +67,14 @@ void loop() {
       case 0x01:
         if (!handleReadCoils(request, response, &responseLength)) return;
         break;
+      case 0x02:
+        if (!handleReadDiscreteInputs(request, response, &responseLength)) return;
+        break;
       case 0x03:
         if (!handleReadHoldingRegisters(request, response, &responseLength)) return;
+        break;
+      case 0x04:
+        if (!handleReadInputRegisters(request, response, &responseLength)) return;
         break;
       case 0x05:
         if (!handleWriteSingleCoil(request, response, &responseLength)) return;
@@ -75,37 +86,31 @@ void loop() {
         if (!handleModbusFunctionException(request[1], response, &responseLength)) return;
         break; 
     }
-
     sendModbusResponse(response, responseLength);
   }
 }
 
 // Função 0x01 - Read Coils 
-// Exemplo de requisição: 01 01 00 01 00 05 C9 AD
+// Exemplo de requisição: 01 01 00 01 00 06 C8 ED | Lê todas as 6 bobinas 
+// Exemplo de requisição: 01 01 00 03 00 04 C9 CD | Lê as 4 últimas bobinas 
+// Exemplo de requisição: 01 01 00 01 00 03 CB 2D | Lê as 3 primeiras bobinas 
 bool handleReadCoils(uint8_t* request, uint8_t* response, uint8_t* responseLength) { 
   uint16_t startAddr = (request[2] << 8) | request[3];
   uint16_t numCoils  = (request[4] << 8) | request[5];
   if(0x0001 > numCoils || numCoils > 0x07D0) return false; // Número de bobinas inválido | CodeError 0x03
-  if(startAddr + numCoils > NUM_REGISTERS) return false;   // Leitura de coils inválida  | CodeError 0x02
+  if((startAddr + numCoils - 1) > NUM_COILS) return false;   // Leitura de coils inválida  | CodeError 0x02
 
   response[0] = SLAVE_ID;
   response[1] = 0x01; // Função de leitura de bobinas
-  response[2] = numCoils / 8 + (numCoils % 8 ? 1 : 0); // Número de bytes a serem enviados
+  response[2] = 1;    // Número de bytes a serem enviados
+  response[3] = 0x00; // Inicializa o primeiro byte de resposta
 
   uint8_t val = 0;
-  PORTB = 0x00010101;
   for(int i = 0; i < numCoils; i++) {
     val = digitalRead(coils[(startAddr - 1) + i]);
-    Serial.print("Pino ");
-    Serial.print(coils[(startAddr - 1) + i]);
-    Serial.print(": ");
-    Serial.println(val);
-    response [3 + i / 8] |= (val << (i % 8));
+    response [3] |= (val << i);
   }
-
-  Serial.print("Response: ");
-  Serial.println(response[3], HEX);
-
+  
   uint16_t crc = calculateCRC(response, 3 + response[2]);
   response[3 + response[2]] = crc & 0xFF;
   response[4 + response[2]] = crc >> 8;
@@ -113,15 +118,29 @@ bool handleReadCoils(uint8_t* request, uint8_t* response, uint8_t* responseLengt
   return true;
 }
 
-bool handleModbusFunctionException(uint8_t functionCode, uint8_t* response, uint8_t* responseLength) {
+// Função 0x02 - Read Discrete Inputs
+// Exemplo de requisição: 01 02 00 00 00 06 08 F8 | Lê todos os 6 inputs digitais
+bool handleReadDiscreteInputs(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
+  uint16_t startAddr = (request[2] << 8) | request[3];
+  uint16_t numInputs  = (request[4] << 8) | request[5];
+  if(0x0000 > numInputs || numInputs > 0x07D0) return false; // Número de inputs inválido 
+  if((startAddr + numInputs) > NUM_DISCRETE_INPUTS) return false;   // Leitura de inputs inválida
+
   response[0] = SLAVE_ID;
-  response[1] = functionCode | 0x80;
-  response[2] = 0x01;
+  response[1] = 0x02; // Função de leitura de inputs digitais
+  response[2] = 0x01; // Número de bytes a serem enviados
+  response[3] = 0x00; // Inicializa o primeiro byte de resposta
   
-  uint16_t crc = calculateCRC(response, 3);
-  response[3] = crc & 0xFF;
-  response[4] = crc >> 8;
-  *responseLength = 5;
+  uint8_t val = 0;
+  for(int i = 0; i < numInputs; i++) {
+    val = digitalRead(inputs[(startAddr) + i]);
+    response [3] |= (val << i);
+  }
+
+  uint16_t crc = calculateCRC(response, 3 + response[2]);
+  response[3 + response[2]] = crc & 0xFF;
+  response[4 + response[2]] = crc >> 8;
+  *responseLength = 5 + response[2];
   return true;
 }
 
@@ -148,25 +167,50 @@ bool handleReadHoldingRegisters(uint8_t* request, uint8_t* response, uint8_t* re
   return true;
 }
 
+// Função 0x04 - Read Input Registers
+// Exemplo de requisição: 01 04 00 00 00 01 CA 31 | Lê o registrador de entrada A0
+// Exemplo de requisição: 01 04 00 00 00 06 08 70 | Lê os registradores de entrada A0 a A5
+bool handleReadInputRegisters(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
+  uint16_t startAddr = (request[2] << 8) | request[3];
+  uint16_t numInputs  = (request[4] << 8) | request[5];
+  if(0x0000 > numInputs || numInputs > 0x07D0) return false; // Número de inputs inválido
+  if((startAddr + numInputs) > NUM_INPUT_REGISTERS) return false;   // Leitura de inputs inválida
+
+  response[0] = SLAVE_ID;
+  response[1] = 0x04; // Função de leitura de registradores de entrada
+  response[2] = numInputs * 2; // Número de bytes a serem enviados
+
+  for (int i = 0; i < numInputs; i++) {
+    uint16_t val = analogRead(inputRegisters[startAddr + i]);
+    response[3 + 2 * i] = val >> 8;
+    response[4 + 2 * i] = val & 0xFF;
+  }
+
+  uint16_t crc = calculateCRC(response, 3 + response[2]);
+  response[3 + response[2]] = crc & 0xFF;
+  response[4 + response[2]] = crc >> 8;
+  *responseLength = 5 + response[2];
+  return true;
+}
+
 // Função 0x05 - Write Single Coil
-// Exemplo de requisição: 01 05 00 00 FF 00 3A 8C
+// Exemplo de requisição: 01 05 00 00 FF 00 3A 8C | Liga o PB0 (D8)
+// Exemplo de requisição: 01 05 00 00 00 00 CA CD | Desliga o PB0 (D8)
+// Exemplo de requisição: 01 05 00 05 FF 00 3B 9C | Liga o PB5 (D13)
+// Exemplo de requisição: 01 05 00 05 00 00 CB DD | Desliga o PB5 (D13)
 bool handleWriteSingleCoil(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
   uint16_t coilAddr = (request[2] << 8) | request[3]; 
   uint16_t value    = (request[4] << 8) | request[5];
-  // Serial.print("Coil Addr: ");
-  // Serial.println(coilAddr, HEX);
-  // Serial.print("Value: ");
-  // Serial.println(value, HEX);
-  if (coilAddr >= NUMCOILS) return false;
+
+  if (coilAddr >= NUM_COILS) return false;
   if (value == 0x0000) {
-    value = 0; // Desliga a bobina
+    value = 0; 
   } else if (value == 0xFF00) {
-    value = 1; // Liga a bobina
+    value = 1;
   } else {
     return false; // Valor inválido
   }
-  // Serial.print("Value: ");
-  // Serial.println(value);
+
   // Atualiza o estado da bobina
   digitalWrite(coils[coilAddr], value);
   if(digitalRead(coils[coilAddr]) != value) {
@@ -183,14 +227,17 @@ bool handleWriteSingleCoil(uint8_t* request, uint8_t* response, uint8_t* respons
   response[7] = crc >> 8;
   *responseLength = 8;
   return true;
-
 }
 
 // Função 0x06 - Write Single Register
 bool handleWriteSingleRegister(uint8_t* request, uint8_t* response, uint8_t* responseLength) {
   uint16_t regAddr = (request[2] << 8) | request[3]; 
   uint16_t value   = (request[4] << 8) | request[5];
-  if (regAddr >= NUM_REGISTERS) return false;  
+  if (regAddr >= NUM_REGISTERS) return false;
+  if (value < 0 || value > 0xFFFF) return false; // Valor inválido
+
+  // Atualiza o valor do registrador
+  holdingRegisters[regAddr] = value;
 
   // Eco da requisição como resposta
   for (int i = 0; i < 6; i++) {
@@ -201,6 +248,18 @@ bool handleWriteSingleRegister(uint8_t* request, uint8_t* response, uint8_t* res
   response[6] = crc & 0xFF;
   response[7] = crc >> 8;
   *responseLength = 8;
+  return true;
+}
+
+bool handleModbusFunctionException(uint8_t functionCode, uint8_t* response, uint8_t* responseLength) {
+  response[0] = SLAVE_ID;
+  response[1] = functionCode | 0x80;
+  response[2] = 0x01;
+  
+  uint16_t crc = calculateCRC(response, 3);
+  response[3] = crc & 0xFF;
+  response[4] = crc >> 8;
+  *responseLength = 5;
   return true;
 }
 
